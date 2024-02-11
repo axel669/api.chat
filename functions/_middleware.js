6b24eaf138f5028e792b7b1a8051aabc33a1d6d7
axel669 <haryuu.hanekata@gmail.com>
2024-02-10 21:59:57 -0800

    [update] change quote style, add post method to cors

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,15 +2,17 @@ export const onRequestOptions = async () => {
     return new Response(null, {
         status: 204,
         headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
         },
     })
 }
 
 export const onRequest = async (context) => {
     const response = await context.next()
-    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set("Access-Control-Allow-Origin", "*")
+    response.headers.set("Access-Control-Allow-Headers", "*")
+    response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS, POST")
     return response
 }
